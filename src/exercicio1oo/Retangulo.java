8cf23062c9196ed22e9be4a60c9953e77eeee202
Jitterkkk <jitterzibetti@gmail.com>
2024-08-26 21:03:34 -0300

    Classe retangulo e respectiva classe de teste

---
+++ src/exercicio1oo/Retangulo.java
@@ -0,0 +1,9 @@
+package exercicio1oo;
+
+public class Retangulo {
+    public int base, altura;
+
+    public void imprimirArea(){
+        System.out.println("Area = " + (base * altura));
+    }
+}
