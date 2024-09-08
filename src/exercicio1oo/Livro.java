ab2e8165dbfbda7c0663562e5c45f236b1866613
Jitterkkk <jitterzibetti@gmail.com>
2024-09-08 00:03:37 -0300

    Livraria Bagualona

---
+++ src/exercicio1oo/Livro.java
@@ -0,0 +1,38 @@
+package exercicio1oo;
+
+public class Livro {
+    public String titulo;
+    public String autor;
+    public boolean emprestado;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.emprestado = false;
+    }
+    public void emprestar(){
+        if (!emprestado) {
+            emprestado = true;
+        } else {
+            System.out.println("O livro já está emprestado");
+        }
+    }
+    public void devolver(){
+        if (emprestado){
+            emprestado = false;
+        } else {
+            System.out.println("O livro já está disponivel")
+        }
+    }
+    public boolean verificarDisponibilidade(){
+        return !emprestado;
+    }
+    @Override
+    public String toString(){
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", emprestado=" + emprestado +
+                '}';
+    }
+}
---
+++ src/exercicio1oo/TestaLivro.java
@@ -0,0 +1,18 @@
+package exercicio1oo;
+
+public class TestaLivro {
+    public static void main(String[] args){
+        Livro livro = new Livro("Diario de um banana", "Jeff Kinney");
+        System.out.println(Livro);
+
+        livro.emprestar();
+        System.out.println("Após emprestar: " + livro);
+
+        livro.emprestar();
+        livro.devolver();
+        System.out.println("Após devolver: " + livro);
+
+        livro.devolver();
+        System.out.println("Disponivel: " + livro.verificarDisponibilidade());
+    }
+}
