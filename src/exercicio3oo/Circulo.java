781b130b990ce735d07eafbc2eb3ba199582432d
Jitterkkk <jitterzibetti@gmail.com>
2024-10-23 15:38:48 -0300

    Circulo ex3 2.0

--- src/exercicio3oo/Circulo.java
+++ src/exercicio3oo/Circulo.java
@@ -3,33 +3,52 @@ package exercicio3oo;
 public class Circulo {
     private double raio;
 
+    // Construtor padrão
     public Circulo() {
-        this.raio = 0;
+        this.raio = 0.0;
     }
 
+    // Construtor com parâmetros
     public Circulo(double raio) {
         this();
         setRaio(raio);
     }
 
-    public double calcularPerimetro() {
-        return 2 * Math.PI * raio;
-    }
-    public double calcularArea(){
-        return Math.PI * Math.pow(raio, 2);
-    }
-
-    public double calcularDiametro() {
-        return 2 * raio;
+    // Método de acesso (getter e setter)
+    public double getRaio() {
+        return raio;
     }
 
     public void setRaio(double raio) {
         if (raio >= 0) {
             this.raio = raio;
+        } else {
+            System.out.println("O raio não pode ser negativo.");
         }
     }
+
+    // Método para calcular a área
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    // Método para calcular o perímetro
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+
+    // Método para calcular o diâmetro
+    public double calcularDiametro() {
+        return 2 * raio;
+    }
+
     @Override
     public String toString() {
-        return "Círculo: Raio = " + raio + ", Área = " + calcularArea() + ", Perímetro = " + calcularPerimetro();
+        return "Círculo{" +
+                "Raio=" + raio +
+                ", Área=" + calcularArea() +
+                ", Perímetro=" + calcularPerimetro() +
+                ", Diâmetro=" + calcularDiametro() +
+                '}';
     }
 }
