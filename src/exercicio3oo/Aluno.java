56620432ccb167c7120f21dc67172b09f1c029c6
Jitterkkk <jitterzibetti@gmail.com>
2024-10-23 14:43:02 -0300

    Aluno ex3 2.0

--- src/exercicio3oo/Aluno.java
+++ src/exercicio3oo/Aluno.java
@@ -1,47 +1,122 @@
 package exercicio3oo;
+import java.util.Scanner;
 
 public class Aluno {
     private String nome;
+    private String materia;
+    private String serie;
     private double nota1;
     private double nota2;
+    private double nota3;
 
     //Construtor padrão
     public Aluno() {
         this.nome = "";
+        this.materia = "";
+        this.serie = "";
         this.nota1 = 0.0;
         this.nota2 = 0.0;
+        this.nota3 = 0.0;
     }
 
     //Construtor parametrizado
-    public Aluno(String nome, double nota1, double nota2) {
+    public Aluno(String nome, String materia, String serie, double nota1, double nota2, double nota3) {
 
 
         this();
-        setNota1(nota1);
-        setNota2(nota2);
         this.nome = nome;
-
-    }
-
-    //Métodos
-    public double calcularMedia(){
-        return (nota1 + nota2) / 2;
-    }
-    public String verificarSituacao(){
-        return calcularMedia() >= 6 ? "Aprovado" : "Reprovado";
-    }
-
-    public void setNota1(double nota1) {
+        this.materia = materia;
+        this.serie = serie;
         this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+
+
+    }
+
+    //Métodos de acesso
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        if (nota3 >= 0 && nota2 <=10){
+            this.nota3 = nota3;
+        } else {
+            System.out.println("Nota inválida. Deve ser entre 0 e 10.");
+        }
+    }
+
+    public double getNota2() {
+        return nota2;
     }
 
     public void setNota2(double nota2) {
-        this.nota2 = nota2;
+        if (nota2 >= 0 && nota2 <=10){
+            this.nota2 = nota2;
+        } else {
+            System.out.println("Nota inválida. Deve ser entre 0 e 10.");
+        }
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        if (nota1 >= 0 && nota1 <=10){
+            this.nota1 = nota1;
+        } else {
+            System.out.println("Nota inválida. Deve ser entre 0 e 10.");
+        }
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Método para calcular a média
+    public double calcularMedia(){
+        return (nota1 + nota2 + nota3) / 3;
+    }
+    // Método para verificar a situação
+    public String verificarSituacao(){
+        double media = calcularMedia();
+        return media >= 6 ? "Aprovado" : "Reprovado";
     }
 
     //toString
     @Override
     public String toString() {
-        return "Aluno: " + nome + ", Média: " + calcularMedia() + ", Situação: " + verificarSituacao();
+        return "Aluno{" +
+                "Nome='" + nome + '\'' +
+                ", Matéria='" + materia + '\'' +
+                ", Série='" + serie + '\'' +
+                ", Nota 1=" + nota1 +
+                ", Nota 2=" + nota2 +
+                ", Nota 3=" + nota3 +
+                ", Média=" + calcularMedia() +
+                ", Situação='" + verificarSituacao() + '\'' +
+                '}';
     }
 }
--- src/exercicio3oo/TestaAluno.java
+++ src/exercicio3oo/TestaAluno.java
@@ -1,13 +1,62 @@
 package exercicio3oo;
-
+import java.util.Scanner;
 public class TestaAluno {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Aluno aluno1 = new Aluno("Bernardo", 8.5, 9.0);
-        System.out.println(aluno1);
+        // Criando o aluno
+        Aluno aluno = new Aluno();
 
-        Aluno aluno2 = new Aluno("Jitter", 7.0, 4.1);
-        System.out.println(aluno2);
+        int opcao;
+        do {
+            System.out.println("\n--- Menu Aluno ---");
+            System.out.println("1. Definir nome, matéria e série");
+            System.out.println("2. Inserir notas");
+            System.out.println("3. Exibir média e situação");
+            System.out.println("4. Exibir informações completas");
+            System.out.println("5. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();  // Limpa o buffer
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Nome do aluno: ");
+                    aluno.setNome(scanner.nextLine());
+                    System.out.print("Matéria: ");
+                    aluno.setMateria(scanner.nextLine());
+                    System.out.print("Série: ");
+                    aluno.setSerie(scanner.nextLine());
+                    break;
+
+                case 2:
+                    System.out.print("Digite a nota 1: ");
+                    aluno.setNota1(scanner.nextDouble());
+                    System.out.print("Digite a nota 2: ");
+                    aluno.setNota2(scanner.nextDouble());
+                    System.out.print("Digite a nota 3: ");
+                    aluno.setNota3(scanner.nextDouble());
+                    break;
+
+                case 3:
+                    System.out.println("Média: " + aluno.calcularMedia());
+                    System.out.println("Situação: " + aluno.verificarSituacao());
+                    break;
+
+                case 4:
+                    System.out.println(aluno);
+                    break;
+
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 5);
+
+        scanner.close();
 
     }
 
